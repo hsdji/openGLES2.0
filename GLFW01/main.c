6ded44cd9749c232f947336d2ec2cb935c041a45
ShanPengFei <2530101715@qq.com>
2021-08-25 17:27:21 +0800

    Initial Commit

---
+++ GLFW01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GLFW01
+//
+//  Created by wuyd on 2021/8/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
